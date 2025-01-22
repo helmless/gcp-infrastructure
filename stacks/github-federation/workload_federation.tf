82476a56bf8509296ccf84c64a908269fe9a8afc
Reichenbach, Michael <755327+Silthus@users.noreply.github.com>
2025-01-22 19:24:50 +0100

    feat: add required permissions to default and repository iam

--- stacks/github-federation/workload_federation.tf
+++ stacks/github-federation/workload_federation.tf
@@ -2,7 +2,8 @@ locals {
   # a list of repositories that are allowed to deploy to Google Cloud Run
   github_organization = "helmless"
   repositories = [
-    "helmless"
+    "helmless",
+    "google-cloudrun"
   ]
 }
 
@@ -18,14 +19,27 @@ locals {
   repository_principals = { for repository in local.repositories : repository => "${module.github_federation.repository_principal_set_id_prefix}/${repository}" }
 }
 
-resource "google_project_iam_member" "project" {
+resource "google_project_iam_member" "run_admin" {
   for_each = local.repository_principals
   project  = data.google_project.project.project_id
   role     = "roles/run.admin"
   member   = each.value
 }
 
-resource "google_service_account_iam_member" "cloud_run_v2" {
+resource "google_project_iam_member" "cloud_deploy" {
+  for_each = local.repository_principals
+  project  = data.google_project.project.project_id
+  role     = "roles/clouddeploy.jobRunner"
+  member   = each.value
+}
+
+resource "google_project_iam_member" "default_service_account_deploy_runner" {
+  project = data.google_project.project.project_id
+  role    = "roles/clouddeploy.jobRunner"
+  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+resource "google_service_account_iam_member" "cloud_run_service_account_user" {
   for_each = local.repository_principals
 
   service_account_id = "projects/${data.google_project.project.project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
