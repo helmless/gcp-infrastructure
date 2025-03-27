59aa110a32018fb3df423ad523c79438c2df42d6
Reichenbach, Michael <755327+Silthus@users.noreply.github.com>
2025-03-27 19:39:38 +0100

    docs: add example annotations for docs

--- stacks/e2e-tests/cloudrun.tf
+++ stacks/e2e-tests/cloudrun.tf
@@ -1,24 +1,27 @@
 locals {
+  github_organization = "helmless"
   repositories = [
-      "helmless",
-      "google-cloudrun-action",
-      "google-cloudrun-charts"
+    "helmless",
+    "google-cloudrun-action",
+    "google-cloudrun-charts"
   ]
-  deployment_accounts = [for repository in local.repositories : module.workload_identity[repository].principal_set]
 }
 
 module "workload_identity" {
   for_each = toset(local.repositories)
-  source = "github.com/helmless/google-workload-identity-federation-terraform-module//repository?ref=v0.2.0"
+  source   = "github.com/helmless/google-workload-identity-federation-terraform-module//repository?ref=v0.2.0"
 
-  repository = "helmless/${each.key}"
+  repository = "${local.github_organization}/${each.key}" # (1)
 }
 
 module "cloudrun_service_e2e_test" {
-  source                 = "github.com/helmless/google-cloudrun-service-terraform-module?ref=v0.1.2"
-  name                   = "full-service"
-  deployment_accounts    = local.deployment_accounts
-  region                 = "europe-west1"
-  create_service_account = true
-  deletion_protection    = false
+  source = "github.com/helmless/google-cloudrun-service-terraform-module?ref=v0.1.2"
+
+  name   = "full-service" # (2)
+  region = "europe-west1"
+
+  deployment_accounts    = values(module.workload_identity).*.principal_set # (3)
+  create_service_account = true # (4)
+
+  deletion_protection = false
 }
